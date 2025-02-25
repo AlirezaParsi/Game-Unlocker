fe30955f356bf5397f37d92755792bbe2c68334c
Aliza <alirezanajmabadi79@gmail.com>
2025-02-25 13:02:47 +0330

    Update GAMEUNLOCKER.java
    Updating code

--- app/src/main/java/io/github/rushiranpise/gameunlocker/GAMEUNLOCKER.java
+++ app/src/main/java/io/github/rushiranpise/gameunlocker/GAMEUNLOCKER.java
@@ -5,224 +5,120 @@ import android.os.Build;
 import android.util.Log;
 
 import java.lang.reflect.Field;
-import java.security.KeyStore;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
-import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
-import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 @SuppressLint("DiscouragedPrivateApi")
-@SuppressWarnings("ConstantConditions")
 public class GAMEUNLOCKER implements IXposedHookLoadPackage {
 
     private static final String TAG = GAMEUNLOCKER.class.getSimpleName();
-    // Packages to Spoof as ROG Phone 6
-    private static final String[] packagesToChangeROG6 = {
-        "com.ea.gp.fifamobile",
-        "com.gameloft.android.ANMP.GloftA9HM",
-        "com.madfingergames.legends",
-        "com.pearlabyss.blackdesertm",
-        "com.pearlabyss.blackdesertm.gl"
-    };
 
-    // Packages to Spoof as Xperia 5
-    private static final String[] packagesToChangeXP5 = {
-        "com.garena.game.codm",
-        "com.tencent.tmgp.kr.codm",
-        "com.vng.codmvn",
-        "com.garena.game.kgvn"
-    };
+    // Map to store package names and their corresponding device profiles
+    private static final Map<String, DeviceProfile> PACKAGE_TO_DEVICE_MAP = new HashMap<>();
 
-    // Packages to Spoof as Xiaomi 13 Pro
-    private static final String[] packagesToChangeOP8P = {
-        "com.netease.lztgglobal",
-        "com.pubg.imobile",
-        "com.pubg.krmobile",
-        "com.rekoo.pubgm",
-        "com.riotgames.league.wildrift",
-        "com.riotgames.league.wildrifttw",
-        "com.riotgames.league.wildriftvn",
-        "com.riotgames.league.teamfighttactics",
-        "com.riotgames.league.teamfighttacticstw",
-        "com.riotgames.league.teamfighttacticsvn",
-        "com.tencent.ig",
-        "com.tencent.tmgp.pubgmhd",
-        "com.vng.pubgmobile",
-        "vng.games.revelation.mobile",
-        "com.ngame.allstar.eu",
-        "com.mojang.minecraftpe",
-        "com.YoStar.AetherGazer",
-        "com.miHoYo.GenshinImpact",
-        "com.garena.game.lmjx"
-    };
+    static {
+        // Asus ROG 6
+        addPackagesToMap(DeviceProfile.ASUS_ROG_6,
+            "com.ea.gp.fifamobile",
+            "com.gameloft.android.ANMP.GloftA9HM",
+            "com.madfingergames.legends",
+            "com.pearlabyss.blackdesertm",
+            "com.pearlabyss.blackdesertm.gl"
+        );
 
-    // Packages to Spoof as OnePlus 9 Pro
-    private static final String[] packagesToChangeOP9P = {
-        "com.epicgames.fortnite",
-        "com.epicgames.portal",
-        "com.tencent.lolm",
-        "jp.konami.pesam"
-    };
+        // Black Shark 4
+        addPackagesToMap(DeviceProfile.BLACK_SHARK_4,
+            "com.proximabeta.mf.uamo"
+        );
 
-    // Packages to Spoof as Mi 11T Pro
-    private static final String[] packagesToChangeMI11TP = {
-        "com.ea.gp.apexlegendsmobilefps",
-        "com.mobilelegends.mi",
-        "com.levelinfinite.hotta.gp",
-        "com.supercell.clashofclans",
-        "com.vng.mlbbvn"
-    };
+        // iQOO 11 Pro
+        addPackagesToMap(DeviceProfile.IQOO_11_PRO,
+            "com.tencent.KiHan",
+            "com.tencent.tmgp.cf",
+            "com.tencent.tmgp.cod",
+            "com.tencent.tmgp.gnyx"
+        );
 
-    // Packages to Spoof as Xiaomi 13 Pro
-    private static final String[] packagesToChangeMI13P = {
-        "com.levelinfinite.sgameGlobal",
-        "com.tencent.tmgp.sgame"
-    };
+        // Xiaomi 13 Pro
+        addPackagesToMap(DeviceProfile.XIAOMI_13_PRO,
+            "com.netease.lztgglobal",
+            "com.pubg.imobile",
+            "com.pubg.krmobile",
+            "com.rekoo.pubgm",
+            "com.riotgames.league.wildrift",
+            "com.riotgames.league.wildrifttw",
+            "com.riotgames.league.wildriftvn",
+            "com.riotgames.league.teamfighttactics",
+            "com.riotgames.league.teamfighttacticstw",
+            "com.riotgames.league.teamfighttacticsvn",
+            "com.tencent.ig",
+            "com.tencent.tmgp.pubgmhd",
+            "com.vng.pubgmobile",
+            "vng.games.revelation.mobile",
+            "com.ngame.allstar.eu",
+            "com.mojang.minecraftpe",
+            "com.YoStar.AetherGazer",
+            "com.miHoYo.GenshinImpact",
+            "com.garena.game.lmjx"
+        );
 
-    // Packages to Spoof as POCO F5
-    private static final String[] packagesToChangeF5 = {
-        "com.dts.freefiremax",
-        "com.dts.freefireth",
-        "com.mobile.legends"
-    };
+        // OnePlus 9 Pro
+        addPackagesToMap(DeviceProfile.ONEPLUS_9_PRO,
+            "com.epicgames.fortnite",
+            "com.epicgames.portal",
+            "com.tencent.lolm",
+            "jp.konami.pesam"
+        );
 
-    // Packages to Spoof as Black Shark 4
-    private static final String[] packagesToChangeBS4 = {
-        "com.proximabeta.mf.uamo"
-    };
+        // Xiaomi Mi 11T Pro
+        addPackagesToMap(DeviceProfile.XIAOMI_MI_11T_PRO,
+            "com.ea.gp.apexlegendsmobilefps",
+            "com.mobilelegends.mi",
+            "com.levelinfinite.hotta.gp",
+            "com.supercell.clashofclans",
+            "com.vng.mlbbvn"
+        );
 
-    // Packages to Spoof as iQOO 11 Pro
-    private static final String[] packagesToChangeiQ11P = {
-        "com.tencent.KiHan",
-        "com.tencent.tmgp.cf",
-        "com.tencent.tmgp.cod",
-        "com.tencent.tmgp.gnyx"
-    };
+        // Poco F5
+        addPackagesToMap(DeviceProfile.POCO_F5,
+            "com.dts.freefiremax",
+            "com.dts.freefireth",
+            "com.mobile.legends"
+        );
+
+        // Lenovo Tablet
+        addPackagesToMap(DeviceProfile.LENOVO_TABLET,
+            "com.garena.game.codm",
+            "com.tencent.tmgp.kr.codm",
+            "com.vng.codmvn",
+            "com.activision.callofduty.shooter",
+            "com.garena.game.kgvn"
+        );
+    }
+
+    // Helper method to add multiple packages to the map
+    private static void addPackagesToMap(DeviceProfile profile, String... packages) {
+        for (String packageName : packages) {
+            PACKAGE_TO_DEVICE_MAP.put(packageName, profile);
+        }
+    }
 
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) {
-
         String packageName = loadPackageParam.packageName;
-
-        // Asus
-        if (Arrays.asList(packagesToChangeROG6).contains(packageName)) {
-            propsToChangeROG6();
-            XposedBridge.log("Spoofed " + packageName + " as Asus ROG 6");
-        }
-
-        // Black Shark
-        if (Arrays.asList(packagesToChangeBS4).contains(packageName)) {
-            propsToChangeBS4();
-            XposedBridge.log("Spoofed " + packageName + " as Black Shark 4");
-        }
-
-        // Iqoo
-        if (Arrays.asList(packagesToChangeiQ11P).contains(packageName)) {
-            propsToChangeiQ11P();
-            XposedBridge.log("Spoofed " + packageName + " as iQOO 11 Pro");
-        }
-
-        // OnePlus
-        if (Arrays.asList(packagesToChangeOP8P).contains(packageName)) {
-            propsToChangeOP8P();
-            XposedBridge.log("Spoofed " + packageName + " as Xiaomi 13 Pro");
-        }
-
-        if (Arrays.asList(packagesToChangeOP9P).contains(packageName)) {
-            propsToChangeOP9P();
-            XposedBridge.log("Spoofed " + packageName + " as OnePlus 9 Pro");
-        }
-
-        // Poco
-        if (Arrays.asList(packagesToChangeF5).contains(packageName)) {
-            propsToChangeF5();
-            XposedBridge.log("Spoofed " + packageName + " as Poco F5");
-        }
-
-        // Sony
-        if (Arrays.asList(packagesToChangeXP5).contains(packageName)) {
-            propsToChangeXP5();
-            XposedBridge.log("Spoofed " + packageName + " as Sony Xperia 5");
-        }
-
-        // Xiaomi
-        if (Arrays.asList(packagesToChangeMI11TP).contains(packageName)) {
-            propsToChangeMI11TP();
-            XposedBridge.log("Spoofed " + packageName + " as Xiaomi Mi 11T Pro");
-        }
-
-        if (Arrays.asList(packagesToChangeMI13P).contains(packageName)) {
-            propsToChangeMI13P();
-            XposedBridge.log("Spoofed " + packageName + " as Xiaomi Mi 13 Pro");
+        DeviceProfile profile = PACKAGE_TO_DEVICE_MAP.get(packageName);
+        if (profile != null) {
+            profile.applyProfile();
+            XposedBridge.log("Spoofed " + packageName + " as " + profile.getDeviceName());
         }
     }
 
-    // Asus
-    // Props to Spoof as Asus Rog 6
-    private static void propsToChangeROG6() {
-        setPropValue("BRAND", "asus");
-        setPropValue("MANUFACTURER", "asus");
-        setPropValue("DEVICE", "AI2201");
-        setPropValue("MODEL", "ASUS_AI2201");
-    }
-
-    // Blackshark
-    // Props to Spoof as Blackshark 4
-    private static void propsToChangeBS4() {
-        setPropValue("MANUFACTURER", "blackshark");
-        setPropValue("MODEL", "2SM-X706B");
-    }
-
-    // Iqoo
-    // Props to Spoof as iQOO 11 Pro
-    private static void propsToChangeiQ11P() {
-        setPropValue("MANUFACTURER", "vivo");
-        setPropValue("MODEL", "V2243A");
-    }
-
-    // OnePlus
-    // Props to Spoof as Xiaomi 13 pro
-    private static void propsToChangeOP8P() {
-        setPropValue("MANUFACTURER", "Xiaomi");
-        setPropValue("MODEL", "2210132G");
-    }
-
-    // Props to Spoof as OnePlus 9 Pro
-    private static void propsToChangeOP9P() {
-        setPropValue("MANUFACTURER", "OnePlus");
-        setPropValue("MODEL", "LE2123");
-    }
-
-    //Poco
-    // Props to Spoof as Poco F5
-    private static void propsToChangeF5() {
-        setPropValue("MANUFACTURER", "Xiaomi");
-        setPropValue("MODEL", "23049PCD8G");
-    }
-
-    // Sony
-    // Props to Spoof as Sony Xperia 5
-    private static void propsToChangeXP5() {
-        setPropValue("MANUFACTURER", "Sony");
-        setPropValue("MODEL", "SO-52A");
-    }
-
-    // Xiaomi
-    // Props to Spoof as Xiaomi Mi 11T Pro
-    private static void propsToChangeMI11TP() {
-        setPropValue("MANUFACTURER", "Xiaomi");
-        setPropValue("MODEL", "2107113SI");
-    }
-
-    // Props to Spoof as Xiaomi Mi 13 Pro
-    private static void propsToChangeMI13P() {
-        setPropValue("MANUFACTURER", "Xiaomi");
-        setPropValue("MODEL", "2210132G");
-    }
-
+    // Method to set system properties using reflection
     private static void setPropValue(String key, Object value) {
         try {
             Log.d(TAG, "Defining prop " + key + " to " + value.toString());
@@ -231,7 +127,41 @@ public class GAMEUNLOCKER implements IXposedHookLoadPackage {
             field.set(null, value);
             field.setAccessible(false);
         } catch (NoSuchFieldException | IllegalAccessException e) {
-            XposedBridge.log("Failed to set prop: " + key + "\n" + Log.getStackTraceString(e));
+            XposedBridge.log("Failed to set prop: " + key + " - " + e.getMessage());
+        }
+    }
+
+    // Enum to manage device profiles
+    public enum DeviceProfile {
+        ASUS_ROG_6("asus", "AI2201", "ASUS_AI2201", "Asus ROG 6"),
+        BLACK_SHARK_4("blackshark", "2SM-X706B", "2SM-X706B", "Black Shark 4"),
+        IQOO_11_PRO("vivo", "V2243A", "V2243A", "iQOO 11 Pro"),
+        XIAOMI_13_PRO("Xiaomi", "2210132G", "2210132G", "Xiaomi 13 Pro"),
+        ONEPLUS_9_PRO("OnePlus", "LE2123", "LE2123", "OnePlus 9 Pro"),
+        XIAOMI_MI_11T_PRO("Xiaomi", "2107113SI", "2107113SI", "Xiaomi Mi 11T Pro"),
+        POCO_F5("Xiaomi", "23049PCD8G", "23049PCD8G", "Poco F5"),
+        LENOVO_TABLET("Lenovo", "TB-9707F", "Lenovo TB-9707F", "Lenovo Tablet");
+
+        private final String manufacturer;
+        private final String device;
+        private final String model;
+        private final String deviceName;
+
+        DeviceProfile(String manufacturer, String device, String model, String deviceName) {
+            this.manufacturer = manufacturer;
+            this.device = device;
+            this.model = model;
+            this.deviceName = deviceName;
+        }
+
+        public void applyProfile() {
+            setPropValue("MANUFACTURER", manufacturer);
+            setPropValue("DEVICE", device);
+            setPropValue("MODEL", model);
+        }
+
+        public String getDeviceName() {
+            return deviceName;
         }
     }
 }
